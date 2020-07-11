08df46441c17bdf67512e1fa69407356248ec2d6
Freddy875 <60365437+Freddy875@users.noreply.github.com>
2020-07-10 20:03:55 -0500

    Create HolaMundo.java
    Un Hola Mundo con Java tiene dos System.out.print o el primero sin salto de linea y el segundo con salto de linea 
    El tercer System.out.println tiene una forma rápida de escribirlo

---
+++ HolaMundo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Freddy875
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("HolaMundo");//Sin salto de linea
+        System.out.println("HolaMundo");//Con salto de linea
+        System.out.println("sout + tab ");
+        /*
+        sout +tab como una forma rapida de
+        escribir el System.out.println();
+        */
+    }//fin public static void main
+    
+}//fin public class HolaMundo
