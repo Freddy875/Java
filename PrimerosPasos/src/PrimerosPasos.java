02249936ab3a6cae17492427bb9cc402f5172eb9
Freddy875 <fer875nandosm@gmail.com>
2020-07-20 20:11:02 -0500

    Creamos un programa en Eclipse sin usar las herramientas de de NetBeans como el Wizard

---
+++ PrimerosPasos/src/PrimerosPasos.java
@@ -0,0 +1,10 @@
+
+public class PrimerosPasos {
+
+	public static void main(String arg[]){
+		
+		System.out.print("Hola estamos dando nuestros primeros pasos en Java");
+		
+	}//fin public static void main
+	
+}//fin public class PrimerosPasos
