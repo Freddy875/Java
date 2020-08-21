3f7cb24e6972abad6fd70d8d57ea602ed9169fc1
Freddy875 <fer875nandosm@gmail.com>
2020-08-21 14:18:13 -0500

    Este programa ordena 3 numeros ingresado por el usuario de mayor a menor

--- Ordenar3NumerosDeMayorAMenor/src/ordenar3numerosdemayoramenor/Ordenar3NumerosDeMayorAMenor.java
+++ Ordenar3NumerosDeMayorAMenor/src/ordenar3numerosdemayoramenor/Ordenar3NumerosDeMayorAMenor.java
@@ -5,9 +5,11 @@
  */
 package ordenar3numerosdemayoramenor;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author fer87
+ * @author Freddy875
  */
 public class Ordenar3NumerosDeMayorAMenor {
 
@@ -15,7 +17,54 @@ public class Ordenar3NumerosDeMayorAMenor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        int iNumero1,iNumero2,iNumero3;
+        
+        iNumero1 = Integer.parseInt(JOptionPane.showInputDialog(
+                "Ingresa el primer número: "));
+        iNumero2 = Integer.parseInt(JOptionPane.showInputDialog( 
+                "Ingresa el segundo número: "));
+        iNumero3 = Integer.parseInt(JOptionPane.showInputDialog( 
+                "Ingresa el tercer número: "));
+        
+        if((iNumero1 > iNumero2) && (iNumero2 > iNumero3)){
+            
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + iNumero1 + "," + iNumero2 + ","  + iNumero3);
+            
+        }else if((iNumero1 > iNumero3) && (iNumero3 > iNumero2)){
+            
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + iNumero1 + "," + iNumero3 + ","  + iNumero2);
+            
+        }else if((iNumero2 > iNumero1) && (iNumero1 > iNumero3)){
+            
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + iNumero2 + "," + iNumero1 + ","  + iNumero3);
+            
+        }else if((iNumero2 > iNumero3) && (iNumero3 >iNumero1)){
+            
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + iNumero2 + "," + iNumero3 + ","  + iNumero1);
+           
+        }else if((iNumero3 > iNumero1) && (iNumero1 > iNumero2)){
+            
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + iNumero3 + "," + iNumero1 + ","  + iNumero2);
+            
+        }else if((iNumero3 > iNumero2) && (iNumero2 > iNumero1)){
+            
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + iNumero3 + "," + iNumero2 + ","  + iNumero1);
+            
+        }else{
+            
+            JOptionPane.showMessageDialog(null,
+                    iNumero1 + "," + iNumero2 + "," + iNumero3 + 
+                            "los tres números son iguales");
+            
+        }
+        
+    }//fin public static void main
     
-}
+}//fin public class Ordenar3NumerosDeMayorAMenor
